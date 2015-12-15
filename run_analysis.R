f6d2ae6882f2ff2473173f0f0f298208ac6b7c35
restrellado <ryan.estrellado@gmail.com>
2015-12-14 21:15:00 -0800

    Mean and std subset, activity descriptions.

--- run_analysis.R
+++ run_analysis.R
@@ -10,10 +10,24 @@ train.act <- read.table("UCI HAR Dataset/train/y_train.txt") # train activities
 train.sub <- read.table("UCI HAR Dataset/train/subject_train.txt") # subjects
 act.label <- read.table("UCI HAR Dataset/activity_labels.txt") # activity labels
 feat <- read.table("UCI HAR Dataset/features.txt") # variable names
-# Add activities, subjects and variable labels to data sets
+# Add activities, subjects and variable labels to data sets.
+# Merge test and training sets.
 x.test <- cbind(x.test, test.act, test.sub)
 colnames(x.test) <- c(as.character(feat[[2]]), "activity", "subject")
 x.train <- cbind(x.train, train.act, train.sub)
 colnames(x.train) <- c(as.character(feat[[2]]), "activity", "subject")
 merged <- rbind(x.train, x.test)
-library(dplyr)
+# Select mean and standard deviation variables
+var.names <- names(merged)
+var.mean <- grep("mean", var.names, value = TRUE)
+var.std <- grep("std", var.names, value = TRUE)
+new.merged <- merged[, c(var.mean, var.std, "activity", "subject")]
+# Replace activity labels with descriptions
+new.merged$activity <- as.character(new.merged$activity)
+new.merged$activity <- sub("1", "walking", new.merged$activity)
+new.merged$activity <- sub("2", "walking upstairs", new.merged$activity)
+new.merged$activity <- sub("3", "walking downstairs", new.merged$activity)
+new.merged$activity <- sub("4", "sitting", new.merged$activity)
+new.merged$activity <- sub("5", "standing", new.merged$activity)
+new.merged$activity <- sub("6", "laying", new.merged$activity)
+# Tidy data set with average of each variable for each activity and each subj.
