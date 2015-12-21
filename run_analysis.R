f4ffad79785e4c05169fd1b2dc7ab8e17b749072
restrellado <ryan.estrellado@gmail.com>
2015-12-21 11:58:13 -0800

    Complete analysis and tidy data set for step 5.

--- run_analysis.R
+++ run_analysis.R
@@ -2,22 +2,22 @@ setwd("~/Documents/github/gcd-course-project")
 file.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(file.url, destfile = "hum_activity.zip", method = "curl")
 unzip("hum_activity.zip")
-x.test <- read.table("UCI HAR Dataset/test/X_test.txt") # test set
-test.act <- read.table("UCI HAR Dataset/test/y_test.txt") # test activities
-test.sub <- read.table("UCI HAR Dataset/test/subject_test.txt") # subjects
-x.train <- read.table("UCI HAR Dataset/train/X_train.txt") # training set
+x.test    <- read.table("UCI HAR Dataset/test/X_test.txt") # test set
+test.act  <- read.table("UCI HAR Dataset/test/y_test.txt") # test activities
+test.sub  <- read.table("UCI HAR Dataset/test/subject_test.txt") # subjects
+x.train   <- read.table("UCI HAR Dataset/train/X_train.txt") # training set
 train.act <- read.table("UCI HAR Dataset/train/y_train.txt") # train activities
 train.sub <- read.table("UCI HAR Dataset/train/subject_train.txt") # subjects
 act.label <- read.table("UCI HAR Dataset/activity_labels.txt") # activity labels
-feat <- read.table("UCI HAR Dataset/features.txt") # variable names
+feat      <- read.table("UCI HAR Dataset/features.txt") # variable names
 # Add activities, subjects and variable labels to data sets.
 # Merge test and training sets.
 x.test <- cbind(x.test, test.act, test.sub)
-colnames(x.test) <- c(as.character(feat[[2]]), "activity", "subject")
 x.train <- cbind(x.train, train.act, train.sub)
+colnames(x.test) <- c(as.character(feat[[2]]), "activity", "subject")
 colnames(x.train) <- c(as.character(feat[[2]]), "activity", "subject")
 merged <- rbind(x.train, x.test)
-# Select mean and standard deviation variables
+# Select only std and mean variables
 var.names <- names(merged)
 var.mean <- grep("mean", var.names, value = TRUE)
 var.std <- grep("std", var.names, value = TRUE)
@@ -30,4 +30,16 @@ new.merged$activity <- sub("3", "walking downstairs", new.merged$activity)
 new.merged$activity <- sub("4", "sitting", new.merged$activity)
 new.merged$activity <- sub("5", "standing", new.merged$activity)
 new.merged$activity <- sub("6", "laying", new.merged$activity)
+# Clean up variable names
+names(new.merged) <- tolower(names(new.merged))
+names(new.merged) <- sub("\\()", "", names(new.merged))
+# Gather sensor variables into one column called type
+library(dplyr)
+library(tidyr)
+new.merged <- tbl_df(new.merged)
+new.merged <- gather(new.merged, type, measurement, -activity, -subject)
 # Tidy data set with average of each variable for each activity and each subj.
+merged.mean <- new.merged %>%
+  group_by(type, activity, subject) %>%
+  summarise(mean = mean(measurement))
+write.table(merged.mean, file = "acc_means.csv", row.names = FALSE)
